14263e08747e1da3ab04cb7dc5e2873ec2746b78
Sergii Podobrii <podobri@ukr.net>
2023-04-07 22:12:07 +0300

    card js

---
+++ src/events/renderGallerycard.js
@@ -0,0 +1,144 @@
+// const BASE_IMG_URL = 'https://image.tmdb.org/t/p/w500';
+
+// export function renderMovieCards(movies) {
+//   const gallery = document.querySelector('.gallery');
+//   const movieGalleryMarkup = movies
+//     .map(movie => createMovieMarkup(movie))
+//     .join('');
+//   gallery.innerHTML = movieGalleryMarkup;
+// }
+
+// export function createMovieMarkup(movie) {
+//   const { title, genre_ids, release_date, poster_path, id } = movie;
+
+//   let year = '';
+//   if (typeof release_date !== 'undefined' && release_date.length > 4) {
+//     year = release_date.slice(0, 4);
+//   }
+
+//   // Get a list of movie genres
+//   const movieGenresList = getMovieGenresList(genre_ids).join(', ');
+
+//   if (poster_path === null) {
+//     return `<li>
+//             <a class="gallery__link" href="#">
+//               <img class="gallery__image" data-id="${id}" src="https://dummyimage.com/395x574/000/fff.jpg&text=no+poster" alt="${title} movie poster" loading="lazy">
+
+//             <div class="info">
+//               <p class="info__item">${title}</p>
+//               <div class="info-detail">
+//                 <p class="info-detail__item">${movieGenresList}</p>
+//                 <p class="info-detail__item">${year}</p>
+//               </div>
+//             </div>
+//             </a>
+//           </li>`;
+//   }
+
+//   return `<li>
+//             <a class="gallery__link" href="#">
+//               <img class="gallery__image" data-id="${id}" src="${BASE_IMG_URL}${poster_path}" alt="${title} movie poster" loading="lazy">
+
+//             <div class="info">
+//               <h3 class="info__item">${title}</h3>
+//               <div class="info-detail">
+//                 <p class="info-detail__item">${movieGenresList}</p>
+//                 <p class="info-detail__item">${year}</p>
+//               </div>
+//             </div>
+//             </a>
+//           </li>`;
+// }
+
+// //  Get a list of movie genres by ID
+// export function getMovieGenresList(genresIdsList) {
+//   let movieGenres = genres.reduce((acc, { id, name }) => {
+//     if (genresIdsList.includes(id)) {
+//       acc.push(name);
+//     }
+//     return acc;
+//   }, []);
+//   if (movieGenres.length > 3) {
+//     movieGenres = movieGenres.slice(0, 2);
+//     movieGenres.push('Other');
+//   }
+//   return movieGenres;
+// }
+
+import ServiceApi from '../services/service-api';
+
+const serviceApi = new ServiceApi();
+const gallery = document.querySelector('.gallery');
+const renderMovieCards = async movies => {
+  const movieGalleryMarkup = await Promise.all(
+    movies
+      .map(async movie => {
+        const { id, title, genre_ids, release_date, backdrop_path } = movie;
+        const genres = await serviceApi.getGenre();
+        const config = await serviceApi.getConfig();
+        const { allGenres } = genres;
+        const { base_url } = config.images;
+
+        let movieGenresList = '';
+        if (allGenres) {
+          movieGenresList = allGenres
+            .filter(item => genre_ids.includes(item['id']))
+            .map(item => item['name'])
+            .join(', ');
+        }
+
+        let year = '';
+        if (typeof release_date !== 'undefined' && release_date.length > 4) {
+          year = release_date.slice(0, 4);
+        }
+
+        const poster_path = backdrop_path
+          ? `${base_url}w440_and_h660_face${backdrop_path}`
+          : 'https://dummyimage.com/395x574/000/fff.jpg&text=no+poster';
+
+        return `<li>
+        <a class="gallery__link" href="#">
+          <img class="gallery__image" data-id="${id}" src="${poster_path}" alt="${title} movie poster" loading="lazy">
+
+        <div class="info">
+          <h3 class="info__item">${title}</h3>
+          <div class="info-detail">
+            <p class="info-detail__item">${movieGenresList}</p>
+            <p class="info-detail__item">${year}</p>
+          </div>
+        </div>
+        </a>
+      </li>`;
+      })
+      .join('')
+  );
+  gallery.innerHTML = movieGalleryMarkup;
+};
+
+// Get a list of movie genres by ID
+export const getMovieGenresList = async genresIdsList => {
+  const genres = await serviceApi.getGenre();
+  const { allGenres } = genres;
+
+  let movieGenres = allGenres.reduce((acc, { id, name }) => {
+    if (genresIdsList.includes(id)) {
+      acc.push(name);
+    }
+    return acc;
+  }, []);
+  if (movieGenres.length > 3) {
+    movieGenres = movieGenres.slice(0, 2);
+    movieGenres.push('Other');
+  }
+  return movieGenres;
+};
+
+serviceApi
+  .getListMovies()
+  .then(movies => {
+    renderMovieCards(movies.listMovies);
+    console.log(movies);
+  })
+  .catch(error => {
+    console.log(error.message);
+  });
--- src/services/service-api.js
+++ src/services/service-api.js
@@ -5,45 +5,48 @@ export default class ServiceApi {
     this.allGenres = null;
     this.config = null;
 
-    this.getGenre().then(({genres}) => {
+    this.getGenre().then(({ genres }) => {
       this.allGenres = genres;
     });
-    this.getConfig().then(({images}) => {
-      this.config = {...images};
+    this.getConfig().then(({ images }) => {
+      this.config = { ...images };
     });
   }
 
   #BASE_URL = 'https://api.themoviedb.org/3';
 
   async getResource(url) {
-    return await fetch(`${this.#BASE_URL}${url}api_key=${API_KEY}`)
-      .then(res => res.json());
+    return await fetch(`${this.#BASE_URL}${url}api_key=${API_KEY}`).then(res =>
+      res.json()
+    );
   }
 
-  getListMovies = async (page=1) => {
+  getListMovies = async (page = 1) => {
     const res = await this.getResource(`/trending/all/day?page=${page}&`);
     return this.transformListMovies(res);
   };
-  getMovie = async (movieId) => {
+  getMovie = async movieId => {
     return await this.getResource(`/movie/${movieId}?`);
   };
-  searchMovie = async (query) => {
+  searchMovie = async query => {
     return await this.getResource(`/search/movie?query=${query}&`);
   };
-  getTrailer = async (movieId) => {
+  getTrailer = async movieId => {
     return await this.getResource(`/movie/${movieId}/videos?`);
   };
-  transformListMovies = (res) => {
+  transformListMovies = res => {
     // console.log(res);
-    const {page, total_results, results} = res;
+    const { page, total_results, results } = res;
 
     const pages = total_results / results.length;
     const totalPages = pages === Math.floor(pages) ? pages : Math.ceil(pages);
 
     const listMovies = results.map(item => {
       console.log(item);
-      const {id, backdrop_path, title, name, release_date, genre_ids} = item;
-      const genres = this.allGenres.filter(item => genre_ids.includes(item['id'])).map(item => item['name']);
+      const { id, backdrop_path, title, name, release_date, genre_ids } = item;
+      const genres = this.allGenres
+        .filter(item => genre_ids.includes(item['id']))
+        .map(item => item['name']);
       const poster = `${this.config['base_url']}w440_and_h660_face${backdrop_path}`;
 
       const d = release_date && new Date(Date.parse(release_date));
@@ -55,7 +58,7 @@ export default class ServiceApi {
         poster,
         release,
         title: title || name,
-      }
+      };
     });
 
     return {
