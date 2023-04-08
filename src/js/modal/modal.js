c2f8adc7788d503b43204c7e021d50e25fbb621a
Artem Liaschenko <playfootball826@gmail.com>
2023-04-08 14:21:27 +0300

    modal for desktop

---
+++ src/js/modal/modal.js
@@ -0,0 +1,35 @@
+const backdrop = document.querySelector('[data-modal]')
+const openButtonModal = document.querySelector('[data-modal-open]')
+const closeButtonModal = document.querySelector('[data-modal-close]')
+
+
+openButtonModal.addEventListener('click', onOpenButtonClick)
+closeButtonModal.addEventListener('click', onCloseButtonClick)
+backdrop.addEventListener('click', onBackdropClicl)
+
+
+function onOpenButtonClick()  {
+  backdrop.classList.remove('is-hidden')
+  window.addEventListener('keydown', closeModalByEscape)
+}
+
+function onCloseButtonClick()  {
+  backdrop.classList.add('is-hidden')
+  window.removeEventListener('keydown', closeModalByEscape)
+}
+
+
+function onBackdropClicl(event) {
+   if (event.currentTarget === event.target) {
+    onCloseButtonClick()
+   }
+ }
+
+
+function closeModalByEscape(event) {
+  const closeButtonEscape = 'Escape'
+  if (event.code === closeButtonEscape) {
+     onCloseButtonClick()
+   }
+}
+ 
