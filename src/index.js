4dc1997642b069fc9735e9cc43b05ee088daeb28
Dmytro Karpenko <kardmitriy@gmail.com>
2023-04-08 16:34:47 +0300

    search-by-keyword comment

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import './js/services/service-api';
 import './js/pagination';
-import './js/services/search-by-keyword';
+// import './js/services/search-by-keyword';
 import './js/events/renderGalleryCard';
