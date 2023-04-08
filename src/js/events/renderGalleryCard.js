d4a9d34bc44a6ad40867547e7cebc0c779b04b68
Yanat58 <tanyalyt80@gmail.com>
2023-04-09 01:03:28 +0300

    add library-gallery.html,scss

--- src/js/events/renderGalleryCard.js
+++ src/js/events/renderGalleryCard.js
@@ -70,6 +70,7 @@ export async function renderMovieCards(movies) {
           <div class="info-detail">
              <p class="info-detail__item">${movieGenres.join(', ')}</p>
              <p class="info-detail__item">${year}</p>
+           </p>
            </div>
          </div>
          </a>
