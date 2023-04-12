6b3f83e7ffa7c9b327338a7dade98a58b89e96b2
classigone@gmail.com <classigone@gmail.com>
2023-04-12 10:38:24 +0300

    team-modal_fix

--- src/js/teamModal.js
+++ src/js/teamModal.js
@@ -19,8 +19,8 @@ const markup = `<div class="team-wrapper">
     <img src="${dmytroUrl}" alt="Dmytro" class="team-image">
     <p class="team-name">Dmytro Karpenko</p>
     <p class="team-role">Team Lead</p>
-    <a href="https://github.com/DmytroKarpenko88" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
-      <use href="${spriteUrl}#github"></use>
+    <a href="https://www.linkedin.com/in/dmytro-karpenko-95a6221a" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
+      <use href="${spriteUrl}#linkedin"></use>
     </svg>
     </a>
 </div>
@@ -28,48 +28,48 @@ const markup = `<div class="team-wrapper">
     <img src="${maiiaUrl}" alt="Maiia" class="team-image">
     <p class="team-name">Maiia Robeiko</p>
     <p class="team-role">Scrum Master</p>
-    <a href="https://github.com/MaiiaHa" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
-      <use href="${spriteUrl}#github"></use>
+    <a href="https://www.linkedin.com/in/maiiaro/" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
+      <use href="${spriteUrl}#linkedin"></use>
     </svg></a>
 </div>
 <div class="team-card">
     <img src="${ruslanUrl}" alt="Ruslan" class="team-image">
     <p class="team-name">Ruslan Boiko</p>
     <p class="team-role">Developer</p>
-    <a href="https://github.com/marseille78" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
-      <use href="${spriteUrl}#github"></use>
+    <a href="https://www.linkedin.com/in/ruslan-boiko/" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
+      <use href="${spriteUrl}#linkedin"></use>
     </svg></a>
 </div>
 <div class="team-card">
     <img src="${halynaUrl}" alt="Halyna" class="team-image">
     <p class="team-name">Halyna Tulchii</p>
     <p class="team-role">Developer</p>
-    <a href="https://github.com/AliaTulchii" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
-      <use href="${spriteUrl}#github"></use>
+    <a href="https://www.linkedin.com/in/halyna-tulchii-869569271/" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
+      <use href="${spriteUrl}#linkedin"></use>
     </svg></a>
 </div>
 <div class="team-card">
     <img src="${mariaUrl}" alt="Maria" class="team-image">
     <p class="team-name">Maria Shymanska</p>
     <p class="team-role">Developer</p>
-    <a href="https://github.com/Maria-Shymanska" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
-      <use href="${spriteUrl}#github"></use>
+    <a href="https://www.linkedin.com/in/maria-shymanska-aa8277251/" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
+      <use href="${spriteUrl}#linkedin"></use>
     </svg></a>
 </div>
 <div class="team-card">
     <img src="${ludmylaUrl}" alt="Ludmyla" class="team-image">
     <p class="team-name">Ludmyla Chumak</p>
     <p class="team-role">Developer</p>
-    <a href="https://github.com/ludmila121" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
-      <use href="${spriteUrl}#github"></use>
+    <a href="https://www.linkedin.com/in/liudmyla-chumak-30732a164/" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
+      <use href="${spriteUrl}#linkedin"></use>
     </svg></a>
 </div>
 <div class="team-card">
     <img src="${artemUrl}" alt="Artem" class="team-image">
     <p class="team-name">Artem Liaschenko</p>
     <p class="team-role">Developer</p>
-    <a href="https://github.com/Artem4535" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
-      <use href="${spriteUrl}#github"></use>
+    <a href="https://www.linkedin.com/in/artem-liaschenko-658819271" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
+      <use href="${spriteUrl}#linkedin"></use>
     </svg></a>
 </div>
 <div class="team-card">
@@ -77,47 +77,47 @@ const markup = `<div class="team-wrapper">
     <p class="team-name">Sergii Podobrii</p>
     <p class="team-role">Developer</p>
     <a href="https://github.com/Mamont777" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
-      <use href="${spriteUrl}#github"></use>
+      <use href="${spriteUrl}#linkedin"></use>
     </svg></a>
 </div>
 <div class="team-card">
     <img src="${andreiiUrl}" alt="Andreii" class="team-image">
     <p class="team-name">Andreii Pokotilo</p>
     <p class="team-role">Developer</p>
-    <a href="https://github.com/AndreyPokotilo" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
-      <use href="${spriteUrl}#github"></use>
+    <a href="https://www.linkedin.com/in/andrii-pokotilo/" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
+      <use href="${spriteUrl}#linkedin"></use>
     </svg></a>
 </div>
 <div class="team-card">
     <img src="${yuraUrl}" alt="Yura" class="team-image">
     <p class="team-name">Yura Doloban</p>
     <p class="team-role">Developer</p>
-    <a href="https://github.com/YuDo-fsd" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
-      <use href="${spriteUrl}#github"></use>
+    <a href="https://www.linkedin.com/in/yurii-doloban-055410254/" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
+      <use href="${spriteUrl}#linkedin"></use>
     </svg></a>
 </div>
 <div class="team-card">
     <img src="${marynaUrl}" alt="Maryna" class="team-image">
     <p class="team-name">Maryna Molchanova</p>
     <p class="team-role">Developer</p>
-    <a href="https://github.com/MarynaMolchanova" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
-      <use href="${spriteUrl}#github"></use>
+    <a href="https://www.linkedin.com/me?trk=p_mwlite_feed_updates-secondary_nav" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
+      <use href="${spriteUrl}#linkedin"></use>
     </svg></a>
 </div>
 <div class="team-card">
     <img src="${tetyanaUrl}" alt="Tetyana" class="team-image">
     <p class="team-name">Tetyana Karlova</p>
     <p class="team-role">Developer</p>
-    <a href="https://github.com/Yanat58" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
-      <use href="${spriteUrl}#github"></use>
+    <a href="https://www.linkedin.com/in/tetiana-karlova-083784270" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
+      <use href="${spriteUrl}#linkedin"></use>
     </svg></a>
 </div>
 <div class="team-card">
     <img src="${kirillUrl}" alt="Kirill" class="team-image">
     <p class="team-name">Kirill Parasochka</p>
     <p class="team-role">Developer</p>
-    <a href="https://github.com/kirill-pka" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
-      <use href="${spriteUrl}#github"></use>
+    <a href="https://www.linkedin.com/in/кирилл-парасочка-825b2b134" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
+      <use href="${spriteUrl}#linkedin"></use>
     </svg></a>
 </div>
 </div>`;
